60b152075d93fa1b105842320d51a8425b6d9e80
Suthiphong .T <suthiphong.t@arise.tech>
2025-12-28 21:01:03 +0700

    Add Ex: validate body with melli

--- src/chachacha/handlers/login.clj
+++ src/chachacha/handlers/login.clj
@@ -2,29 +2,24 @@
   (:require [malli.core :as m]
             [malli.error :as me]))
 
-(def login-schema
+(def LoginSchema
   [:map
-   [:email [:re {:error/message "Invalid email format"}
-            #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"]]
-   [:password [:string {:min 8 :error/message "Password must be at least 8 characters"}]]])
-
-(defn validate-login-request
-  [data]
-  (if (m/validate login-schema data)
-    {:valid? true :data data}
-    {:valid? false
-     :errors (-> login-schema
-                 (m/explain data)
-                 (me/humanize))}))
+   [:email [:string {:min 3}]]
+   [:password [:string {:min 8}]]])
 
 (defn login-handler
   [request]
-  (let [body (:body-params request)
-        validation (validate-login-request body)]
-    (if (:valid? validation)
-      (let [{:keys [email password]} (:data validation)]
-        (println "email:" email "password:" password)
+  (let [body (:body-params request)]
+
+    (println "request body:" body)
+
+    (if (m/validate LoginSchema body)
+      (let [email (:email body)
+            password (:password body)]
+        (println "email " email "password: " password)
+
         {:status 200
          :body {:message "Login received"}})
       {:status 400
-       :body {:errors (:errors validation)}})))
+       :body {:message "Invalid request"}})))
+
